a5907526b8500fcf0c5b2a39358b0c45673c3c33
tangpeng <1084165470@qq.com>
2016-10-24 21:52:47 +0800

    先提交java文件来试试

--- auxiliaryteachingapp/src/main/java/com/example/tp/auxiliaryteachingapp/MainActivity.java
+++ auxiliaryteachingapp/src/main/java/com/example/tp/auxiliaryteachingapp/MainActivity.java
@@ -10,5 +10,6 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         System.out.println("这是一句测试的话：for github");
+        System.out.println("奇怪~~");
     }
 }
