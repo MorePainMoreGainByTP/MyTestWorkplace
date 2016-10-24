d51b118f42160d05fac445cde6f161d3709b393a
tangpeng <1084165470@qq.com>
2016-10-24 21:37:41 +0800

    先提交java文件来试试

---
+++ auxiliaryteachingapp/src/main/java/com/example/tp/auxiliaryteachingapp/MainActivity.java
@@ -0,0 +1,14 @@
+package com.example.tp.auxiliaryteachingapp;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        System.out.println("这是一句测试的话：for github");
+    }
+}
