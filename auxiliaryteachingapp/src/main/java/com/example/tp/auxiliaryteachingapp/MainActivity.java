c543ce8e1bfce4f089715d8c4662e0f7096945ce
tangpeng <1084165470@qq.com>
2016-10-24 22:27:14 +0800

    全部提交

--- auxiliaryteachingapp/src/main/java/com/example/tp/auxiliaryteachingapp/MainActivity.java
+++ auxiliaryteachingapp/src/main/java/com/example/tp/auxiliaryteachingapp/MainActivity.java
@@ -9,10 +9,6 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        System.out.println("这是一句测试的话：for github");
-        System.out.println("奇怪~~");
-        System.out.println("haha");
-        System.out.println("怎么回事");
-        System.out.println("dkjcbhsjdb");
+        System.out.println("马丹，终于弄好了！！！");
     }
 }
